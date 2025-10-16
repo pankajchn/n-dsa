58e4db3ca0b26943fee5270a4c804eca69f06caf
pankajchn <realpankajchouhan08@gmail.com>
2025-10-16 22:23:21 +0530

    Add: implement right-aligned star pattern printing

---
+++ pattern_printing/p7.js
@@ -0,0 +1,15 @@
+
+
+
+const n = 5;
+
+for ( let i = 0; i < n; i++ ) {
+    let row = "";
+    for ( let j = 0; j < n-(i+1); j++ ) {
+        row = row + " ";
+    }
+    for ( let k = 0; k < i+1; k++ ) {
+        row = row + "*"
+    }
+    console.log(row);
+}
